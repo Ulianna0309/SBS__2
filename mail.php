5f3334a6b33414aa359eafdc74659cc87dca6187
Ulianna0309 <ulianna0309@gmail.com>
2020-11-07 19:57:11 +0200

    real price without form action

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 
 <meta charset="utf-8"> 
 <?php
-$site="SBS";
+$urok="Урок 22";
 error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
 // создание переменных из полей формы		
 if (isset($_POST['name1']))			{$name1			= $_POST['name1'];		if ($name1 == '')	{unset($name1);}}
@@ -23,12 +23,12 @@ $text=stripslashes($text);
 $text=htmlspecialchars($text);
 }
 // адрес почты куда придет письмо
-$address="adminwm@mail.ru";
+$address="24navo@gmail.com";
 // текст письма 
-$note_text="Тема : $site \r\nИмя : $name1 \r\n Email : $email1 \r\n Дополнительная информация : $text";
+$note_text="Тема : $urok \r\nИмя : $name1 \r\n Email : $email1 \r\n Дополнительная информация : $text";
 
 if (isset($name1)  &&  isset ($sab) ) {
-mail($address,$site,$note_text,"Content-type:text/plain; windows-1251"); 
+mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
 // сообщение после отправки формы
     
 echo "<p style='color:green;'>Уважаемый(ая) <b style='color:red;'>$name1</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b style='color:red;'> $email1</b>.</p>";
