d68dc868972b6089c824c63dd07dcab0671f9b02
Ulianna0309 <ulianna0309@gmail.com>
2020-11-08 01:40:13 +0200

    form

--- js/main.js
+++ js/main.js
@@ -75,3 +75,23 @@ $(function() {
     scrollTo();
 }());
 
+$(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Спасибо!Мы с вами свяжемся в ближайшее время!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
--- js/qiuz.js
+++ js/qiuz.js
@@ -319,5 +319,24 @@ $(document).ready(function() {
     
 });
 
+$(document).ready(function() {
 
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "send.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Спасибо!Мы с вами свяжемся в ближайшее время!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
 
